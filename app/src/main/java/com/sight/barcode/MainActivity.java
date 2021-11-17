f8100a843343e6fc34ffb7b43cd98c54c93e3a3c
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-17 16:59:39 -0300

    Initial commit

---
+++ app/src/main/java/com/sight/barcode/MainActivity.java
@@ -0,0 +1,213 @@
+package com.sight.barcode;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.LifecycleOwner;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Size;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.barcode.Barcode;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    //permissões
+    private String[] permissoes = new String[]{
+            Manifest.permission.CAMERA
+    };
+
+    private PreviewView previewView;
+    private ListenableFuture cameraProviderFuture;
+    private ExecutorService cameraExecutor;
+    private MyImageAnalyzer analyzer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /**validar permissões*/
+        Permissoes.validarPermissoes(permissoes, this, 1);
+        previewView = findViewById(R.id.previewView);
+        setCameraProviderListener();
+
+        analyzer = new MyImageAnalyzer(getSupportFragmentManager());
+    }
+
+    private void setCameraProviderListener() {
+        cameraExecutor = Executors.newSingleThreadExecutor();
+        cameraProviderFuture = ProcessCameraProvider.getInstance(MainActivity.this);
+
+        cameraProviderFuture.addListener(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ProcessCameraProvider processCameraProvider = (ProcessCameraProvider) cameraProviderFuture.get();
+                    bindPreview(processCameraProvider);
+                }catch (ExecutionException e) {
+                    e.printStackTrace();
+                }catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, ContextCompat.getMainExecutor(MainActivity.this));
+    }
+
+    public void bindPreview(ProcessCameraProvider cameraProvider) {
+
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+        ImageCapture imageCapture = new ImageCapture.Builder().build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                .setTargetResolution(new Size(1200, 720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build();
+        imageAnalysis.setAnalyzer(cameraExecutor, analyzer);
+        cameraProvider.unbindAll();
+        //preview.setSurfaceProvider(previewView.getSurfaceProvider());
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, imageAnalysis);
+    }
+
+    public class MyImageAnalyzer implements ImageAnalysis.Analyzer {
+        private FragmentManager fragmentManager;
+        private bottom_dialog bd;
+
+        public MyImageAnalyzer(FragmentManager fragmentManager) {
+            this.fragmentManager = fragmentManager;
+            bd = new bottom_dialog();
+        }
+
+        @Override
+        public void analyze(@NonNull ImageProxy image) {
+            scanbarcode(image);
+        }
+
+
+        private void scanbarcode(ImageProxy imageProxy) {
+
+            @SuppressLint("UnsafeOptInUsageError") Image image = imageProxy.getImage();
+            assert image != null;
+            InputImage inputImage = InputImage.fromMediaImage(image, imageProxy.getImageInfo().getRotationDegrees());
+            BarcodeScannerOptions options =
+                    new BarcodeScannerOptions.Builder()
+                            .setBarcodeFormats(
+                                    Barcode.FORMAT_QR_CODE,
+                                    Barcode.FORMAT_AZTEC)
+                            .build();
+
+            BarcodeScanner scanner = BarcodeScanning.getClient(options);
+            Task<List<Barcode>> result = scanner.process(inputImage)
+                    .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                        @Override
+                        public void onSuccess(@NonNull List<Barcode> barcodes) {
+                            readerBarcodeData(barcodes);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    })
+                    .addOnCompleteListener(new OnCompleteListener<List<Barcode>>() {
+                        @Override
+                        public void onComplete(@NonNull Task<List<Barcode>> task) {
+                            image.close();
+                        }
+                    });
+
+        }
+
+        private void readerBarcodeData(List<Barcode> barcodes) {
+            for (Barcode barcode: barcodes) {
+                Rect bounds = barcode.getBoundingBox();
+                Point[] corners = barcode.getCornerPoints();
+
+                String rawValue = barcode.getRawValue();
+
+                int valueType = barcode.getValueType();
+                // See API reference for complete list of supported types
+                switch (valueType) {
+                    case Barcode.TYPE_WIFI:
+                        String ssid = barcode.getWifi().getSsid();
+                        String password = barcode.getWifi().getPassword();
+                        int type = barcode.getWifi().getEncryptionType();
+                        break;
+                    case Barcode.TYPE_URL:
+                        if (bd.isAdded()){
+                            bd.show(fragmentManager, "");
+                        }
+                        bd.fetchurl(barcode.getUrl().getUrl());
+
+                        String title = barcode.getUrl().getTitle();
+                        String url = barcode.getUrl().getUrl();
+                        break;
+                }
+            }
+        }
+
+    }
+
+    /**Metodo que percorre permissões no manifest*/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResult : grantResults){
+            if (permissaoResult == PackageManager.PERMISSION_DENIED){
+                alertaPermissaoNegada();
+            }
+        }
+    }
+
+    /**Alert Dialoq da permissão da camera*/
+    private void alertaPermissaoNegada(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para que o geduxEstoque funcione corretamente é necessario aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
